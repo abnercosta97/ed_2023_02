afb96a95031d0152a9fc851d09e4cfceecf1fb5e
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-18 19:59:41 -0300

    Aula modelo e visao

---
+++ src/visao.ts
@@ -0,0 +1,18 @@
+import {somar, subtrair, despedir} from "./modelo"
+
+let escolher:string;
+escolher = "somar";
+let resultado:number;
+
+switch (escolher){
+    case "somar":
+        resultado = somar(1,2);
+        console.log(resultado);
+        break;
+    case "subtrair":
+        resultado = subtrair(1,2);
+        console.log(resultado);
+        break;
+    case "despedir":
+        console.log(despedir());
+}
