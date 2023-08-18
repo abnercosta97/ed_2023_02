ca2bc7e11625658f71e20523bc5f9eded6fb17f4
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-18 20:54:24 -0300

    prettier

--- src/visao-prompt.ts
+++ src/visao-prompt.ts
@@ -1,26 +1,32 @@
-import {somar, subtrair, despedir} from "./modelo"
+import { somar, subtrair, despedir } from "./modelo";
 
 import prompt_sync from "prompt-sync";
 
 const prompt = prompt_sync();
 let opcao: string = prompt("Digite sua opção: ");
 
-let escolher:string;
+let escolher: string;
 escolher = opcao;
-let resultado:number;
+let resultado: number;
 
-switch (escolher){
-    case "somar":
-        resultado = somar(parseInt(prompt("Digite um número:")), parseInt(prompt("Digite outro número:")));
-        console.log("Resultado da soma é:", resultado);
-        break;
-    case "subtrair":
-        resultado = subtrair(parseInt(prompt("Digite um número:")), parseInt(prompt("Digite outro número:")));
-        console.log("Resultado da subtração é:", resultado);
-        break;
-    case "despedir":
-        console.log(despedir());
+switch (escolher) {
+  case "somar":
+    resultado = somar(
+      parseInt(prompt("Digite um número:")),
+      parseInt(prompt("Digite outro número:"))
+    );
+    console.log("Resultado da soma é:", resultado);
+    break;
+  case "subtrair":
+    resultado = subtrair(
+      parseInt(prompt("Digite um número:")),
+      parseInt(prompt("Digite outro número:"))
+    );
+    console.log("Resultado da subtração é:", resultado);
+    break;
+  case "despedir":
+    console.log(despedir());
 }
 
-// Visão é ponto de interação com usuário. 
+// Visão é ponto de interação com usuário.
 // MVC (Modelo, Visão, Controlador)
