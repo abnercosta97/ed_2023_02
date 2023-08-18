3f478f73587e86f33632b7eef5590c1ac578f674
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-18 20:50:03 -0300

    atual

--- src/visao-prompt.ts
+++ src/visao-prompt.ts
@@ -12,11 +12,11 @@ let resultado:number;
 switch (escolher){
     case "somar":
         resultado = somar(parseInt(prompt("Digite um número:")), parseInt(prompt("Digite outro número:")));
-        console.log(resultado);
+        console.log("Resultado da soma é:", resultado);
         break;
     case "subtrair":
         resultado = subtrair(parseInt(prompt("Digite um número:")), parseInt(prompt("Digite outro número:")));
-        console.log(resultado);
+        console.log("Resultado da subtração é:", resultado);
         break;
     case "despedir":
         console.log(despedir());
