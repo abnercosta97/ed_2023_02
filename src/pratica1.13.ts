b3d6454a90661572db5d1ecd2b9a7bc231aede14
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-27 14:36:41 -0300

    exercicio 1.13

--- src/pratica1.13.ts
+++ src/pratica1.13.ts
@@ -1,5 +1,42 @@
 class Usuario {
     nome: string;
+    nascimento: string;
+    cpf: string;
+    genero: string;
+    
+    constructor(nome: string, nascimento: string, cpf: string, genero: string){
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.cpf = cpf;
+        this.genero = genero;
+    }
+
+    speak_name(): void {
+        console.log(`Usuário: ${this.nome}.`);
+    }
+
+    equals(outroUsuario: Usuario): boolean {
+        return (
+            this.nome === outroUsuario.nome &&
+            this.nascimento === outroUsuario.nascimento &&
+            this.cpf === outroUsuario.cpf &&
+            this.genero === outroUsuario.genero
+        );
+    }
+}
+
+// Exemplo de uso da classe
+const usuario1 = new Usuario("Abner", "06/04/1997", "123.456.789-00", "Masculino");
+const usuario2 = new Usuario("Jessica", "20/09/1993", "987.654.321-00", "Feminino");
+
+usuario1.speak_name();
+usuario2.speak_name();
+
+console.log(usuario1.equals(usuario2)); // Exemplo de uso do método equals
+
+/*
+class Usuario {
+    nome: string;
     nascimento: Date;
     cpf: string; // Alterei para string, pois o CPF pode ter caracteres não numéricos
     genero: string;
@@ -33,8 +70,7 @@ usuario1.speak_name();
 usuario2.speak_name();
 
 console.log(usuario1.equals(usuario2)); // Exemplo de uso do método equals
-
-
+*/
 
 
 
