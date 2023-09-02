7fbbbd94edf208ad1816f545610d34f99d188d25
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-09-02 18:05:11 -0300

    exercicio 2.1

--- unidade2/exer2_1.ts
+++ unidade2/exer2_1.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+function max(array:number[]):number {
+    if (array.length === 2) return array[0] > array[1] ? array[0] : array[1];
+    let sub_max = max(array.slice(1));
+    return array[0] > sub_max ? array[0] : sub_max;
+  }
+  
+
+function alternativeSolutionMax(array: number[], max = 0):number {
+    return array.length === 0
+      ? max
+      : alternativeSolutionMax(array.slice(1), array[0] > max ? array[0] : max);
+  }
+
+console.log(max([1, 5, 10, 25, 16, 1])); // 25
+console.log(alternativeSolutionMax([1, 5, 10, 25, 16, 1])); // 25
