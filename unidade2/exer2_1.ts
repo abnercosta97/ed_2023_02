4c00deb4d2fb5e2b19cfdacd1261b2254817323d
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-09-22 17:54:26 -0300

    comentando metodo slice

--- unidade2/exer2_1.ts
+++ unidade2/exer2_1.ts
@@ -13,3 +13,10 @@ function alternativeSolutionMax(array: number[], max = 0):number {
 
 console.log(max([1, 5, 10, 25, 16, 1])); // 25
 console.log(alternativeSolutionMax([1, 5, 10, 25, 16, 1])); // 25
+
+/*            Array.prototype.slice()
+O método slice() retorna uma cópia de parte de um array a partir de um subarray criado entre as posições início e fim (fim não é incluído) de um array original. O Array original não é modificado.
+
+
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+*/
