c6896c808044dc85b9e0f346ef94fe57c35c070f
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-09-22 19:36:55 -0300

    ok

--- unidade2/exer2_1.ts
+++ unidade2/exer2_1.ts
@@ -1,7 +1,7 @@
-function max(array:number[]):number {
-    if (array.length === 2) return array[0] > array[1] ? array[0] : array[1];
-    let sub_max = max(array.slice(1));
-    return array[0] > sub_max ? array[0] : sub_max;
+function min(array:number[]):number {
+    if (array.length === 2) return array[0] < array[1] ? array[0] : array[1];
+    let sub_min = min(array.slice(1));
+    return array[0] < sub_min ? array[0] : sub_min;
   }
   
 
@@ -11,8 +11,8 @@ function alternativeSolutionMax(array: number[], max = 0):number {
       : alternativeSolutionMax(array.slice(1), array[0] > max ? array[0] : max);
   }
 
-console.log(max([1, 5, 10, 25, 16, 1])); // 25
-console.log(alternativeSolutionMax([1, 5, 10, 25, 16, 1])); // 25
+console.log(min([1, 5, 10, 25, 16, 18])); // 1
+console.log(alternativeSolutionMax([1, 5, 10, 25, 16, 18])); // 25
 
 /*            Array.prototype.slice()
 O método slice() retorna uma cópia de parte de um array a partir de um subarray criado entre as posições início e fim (fim não é incluído) de um array original. O Array original não é modificado.
--- unidade2/tc2_2.ts
+++ unidade2/tc2_2.ts
@@ -28,5 +28,28 @@ function fibonacci(n: number): number {
 const resultadoFibonacci = fibonacci(5);
 console.log(resultadoFibonacci); // Saída: 5
 
+/* 
+Cálculo de fatorial(5):
+
+Chamada inicial: fatorial(5)
+fatorial(5) chama fatorial(4)
+fatorial(4) chama fatorial(3)
+fatorial(3) chama fatorial(2)
+fatorial(2) chama fatorial(1)
+fatorial(1) chama fatorial(0)
+Neste caso, o algoritmo de cálculo do fatorial tem uma pilha de chamadas recursivas que cresce linearmente com o valor de entrada n. Portanto, a complexidade espacial é O(n).
+
+Cálculo de fibonacci(5):
+
+Chamada inicial: fibonacci(5)
+fibonacci(5) chama fibonacci(4) e fibonacci(3)
+fibonacci(4) chama fibonacci(3) e fibonacci(2)
+fibonacci(3) chama fibonacci(2) e fibonacci(1)
+fibonacci(2) chama fibonacci(1) e fibonacci(0)
+Neste caso, o algoritmo de cálculo de Fibonacci tem uma pilha de chamadas recursivas que cresce exponencialmente com o valor de entrada n. Isso ocorre porque, para calcular fibonacci(n), ele chama fibonacci(n-1) e fibonacci(n-2), resultando em uma árvore de chamadas recursivas de profundidade exponencial.
+
+Portanto, a complexidade espacial do algoritmo de Fibonacci é O(2^n), o que é significativamente maior do que a complexidade espacial do algoritmo de cálculo do fatorial. Isso ilustra que nem todos os algoritmos recursivos têm a mesma complexidade espacial, e é importante escolher algoritmos que sejam eficientes em termos de uso de memória, quando apropriado.
+*/
+
 
 
